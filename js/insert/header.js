1b3ac3cab7a5ce81c27f3f1323fff6fed6361f40
Asher <32278830+athaun@users.noreply.github.com>
2021-06-18 21:29:54 -0400

    landing page, background image

--- js/insert/header.js
+++ js/insert/header.js
@@ -9,9 +9,11 @@ function setHeader (header, subheader) {
     
     welcomeHeader = `
     <!-- Welcome Sign -->
+    <div class="welcomeParent">
     <div class="welcome">
         <h1>` + header + `</h1>
-        <p>` + subheader + `</p>
+        <div style="text-align: center; width:100%;"><a class="button c2a" target="_blank" href="https://azurite-engine.github.io/Azurite-Docs/tutorials/set-a-project.html">Start using Azurite!</a></div>
+    </div>
     </div>
     `;
 
